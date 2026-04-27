357c6d8d0579d2a76fea9310bd9264112f59a77c
Adan Siqueira <adan_siqueira@live.com>
2026-04-27 19:56:20 -0300

    feat(component): implement Collaboration component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./components/Benefits";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero"
+import Collaboration from "./components/Collaboration";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Hero/>
         <Benefits/>
+        <Collaboration />
       </div>
 
       <ButtonGradient />   
